9cc96e81f53a9e66ac084be0dc32bea4d14fc052
Hee-Cheol Kim <41623476+dogfoots@users.noreply.github.com>
2023-06-28 00:27:24 +0900

    1
    1

--- lib/screens/swiping_cards_screen.dart
+++ lib/screens/swiping_cards_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class SwipingCardsScreen extends StatefulWidget {
@@ -10,29 +12,42 @@ class SwipingCardsScreen extends StatefulWidget {
 class _SwipingCardsScreenState extends State<SwipingCardsScreen>
     with SingleTickerProviderStateMixin {
   late final size = MediaQuery.of(context).size;
-  late final AnimationController _animationController = AnimationController(
+  late final AnimationController _position = AnimationController(
       vsync: this,
-      lowerBound: -1.0 * size.width,
-      upperBound: size.width,
+      lowerBound: -1.0 * (size.width + 100),
+      upperBound: (size.width + 100),
       value: 0.0,
       duration: Duration(
         milliseconds: 300,
       ));
 
+  late final Tween<double> _rotation = Tween(
+    begin: -15,
+    end: 15,
+  );
+
   void _onHorizontalDragUpdate(DragUpdateDetails details) {
-    _animationController.value += details.delta.dx;
+    _position.value += details.delta.dx;
   }
 
   void _onHorizontalDragEnd(DragEndDetails details) {
-    _animationController.animateTo(
-      0,
-      curve: Curves.bounceOut,
-    );
+    final bound = size.width - 100;
+    if (_position.value.abs() >= bound) {
+      _position.animateTo(
+        (_position.value.isNegative ? -1.0 : 1.0) * (size.width + 100),
+        curve: Curves.bounceOut,
+      );
+    } else {
+      _position.animateTo(
+        0,
+        curve: Curves.bounceOut,
+      );
+    }
   }
 
   @override
   void dispose() {
-    _animationController.dispose();
+    _position.dispose();
     super.dispose();
   }
 
@@ -43,22 +58,27 @@ class _SwipingCardsScreenState extends State<SwipingCardsScreen>
         title: Text('Swiping Cards'),
       ),
       body: AnimatedBuilder(
-        animation: _animationController,
+        animation: _position,
         builder: (context, child) {
-          return Stack(children: [
-            Align(
-              alignment: Alignment.topCenter,
+          final angle = _rotation
+              .transform((_position.value + (size.width / 2)) / size.width);
+          return Stack(alignment: Alignment.topCenter, children: [
+            Positioned(
+              top: 100,
               child: GestureDetector(
                 onHorizontalDragUpdate: _onHorizontalDragUpdate,
                 onHorizontalDragEnd: _onHorizontalDragEnd,
                 child: Transform.translate(
-                  offset: Offset(_animationController.value, 0),
-                  child: Material(
-                    elevation: 10,
-                    color: Colors.red.shade100,
-                    child: SizedBox(
-                      width: size.width * 0.8,
-                      height: size.height * 0.5,
+                  offset: Offset(_position.value, 0),
+                  child: Transform.rotate(
+                    angle: angle * pi / 180.0,
+                    child: Material(
+                      elevation: 10,
+                      color: Colors.red.shade100,
+                      child: SizedBox(
+                        width: size.width * 0.8,
+                        height: size.height * 0.5,
+                      ),
                     ),
                   ),
                 ),
